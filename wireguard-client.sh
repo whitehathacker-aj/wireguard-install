cc6e0da26446d8907a237f97f06ea37473dbb444
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-03-23 11:11:24 -0400

    Update wireguard-client.sh

--- wireguard-client.sh
+++ wireguard-client.sh
@@ -34,7 +34,7 @@ fi
         add-apt-repository ppa:wireguard/wireguard -y
         apt-get update
         apt-get install wireguard resolvconf -y
-        
+
     elif [ "$DISTRO" == "Debian" ]; then
         echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
         printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
