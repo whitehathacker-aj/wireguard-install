a05a2f5d6edf2a895487b084e566a3131a8bdad3
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-04-14 13:56:49 -0400

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -30,6 +30,59 @@ if [ "$( systemd-detect-virt )" == "openvz" ]; then
     exit
 fi
 
+function installUnbound () {
+    if [[ ! -e /etc/unbound/unbound.conf ]]; then
+ 
+        if [[ "$OS" =~ (debian|ubuntu) ]]; then
+            apt-get install unbound unbound-host -y
+            wget /var/lib/unbound/root.hints https://www.internic.net/domain/named.cache
+            cp /usr/share/dns/root.key /var/lib/unbound/
+            systemctl disable systemd-resolved.service
+            service systemd-resolved stop
+    echo "" > /etc/unbound/unbound.conf
+    echo "server:
+  num-threads: 4
+  do-ip6: no  # Disable ip6 for Azure
+  #Enable logs
+  verbosity: 1
+  #list of Root DNS Server
+  root-hints: "/var/lib/unbound/root.hints"
+  #Use the root servers key for DNSSEC
+  auto-trust-anchor-file: "/var/lib/unbound/root.key"
+  #Respond to DNS requests on all interfaces
+  interface: 0.0.0.0
+  max-udp-size: 3072
+  #Authorized IPs to access the DNS Server
+  access-control: 0.0.0.0/0                 refuse
+  access-control: 127.0.0.1                 allow
+  access-control: 10.0.0.0/24               allow
+  #not allowed to be returned for public internet  names
+  private-address: 10.0.0.0/24
+  # Hide DNS Server info
+  hide-identity: yes
+  hide-version: yes
+  #Limit DNS Fraud and use DNSSEC
+  harden-glue: yes
+  harden-dnssec-stripped: yes
+  harden-referral-path: yes
+  #Add an unwanted reply threshold to clean the cache and avoid when possible a DNS Poisoning
+  unwanted-reply-threshold: 10000000
+  #Have the validator print validation failures to the log.
+  val-log-level: 1
+  #Minimum lifetime of cache entries in seconds
+  cache-min-ttl: 1800
+  #Maximum lifetime of cached entries
+  cache-max-ttl: 14400
+  prefetch: yes
+  prefetch-key: yes" > /etc/unbound/unbound.conf
+        chown -R unbound:unbound /var/lib/unbound
+        iptables -A INPUT -s 10.0.0.0/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+ 
+    fi
+                    systemctl enable unbound
+                    service unbound restart
+}
+
 if [ ! -f "$WG_CONFIG" ]; then
     ### Install server and add default client
     INTERACTIVE=${INTERACTIVE:-yes}
@@ -153,6 +206,9 @@ if [ ! -f "$WG_CONFIG" ]; then
         esac
         
     fi
+    
+    	if [[ $DNS == 11 ]];then
+		installUnbound
 
     if [ "$DISTRO" == "Ubuntu" ]; then
         apt-get update
