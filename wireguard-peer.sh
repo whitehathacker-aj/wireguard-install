95847f9dcdf89d287300fe4db3c782c0ac65e886
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-04-20 19:00:50 -0400

    Create wireguard-peer.sh

---
+++ wireguard-peer.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# https://github.com/LiveChief/wireguard-install
+# Secure WireGuard Client installer for Debian, Ubuntu, CentOS
+#
+
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Sorry, you need to run this as root"
+    exit
+fi
+
+if [ -e /etc/centos-release ]; then
+    DISTRO="CentOS"
+elif [ -e /etc/debian_version ]; then
+    DISTRO=$( lsb_release -is )
+else
+    echo "Your distribution is not supported (yet)"
+    exit
+fi
+
+if [ "$( systemd-detect-virt )" == "openvz" ]; then
+    echo "OpenVZ virtualization is not supported"
+    exit
+fi
+
+    if [ "$DISTRO" == "Ubuntu" ]; then
+        apt-get update
+        apt-get upgrade -y
+        apt-get dist-upgrade -y
+        apt-get autoremove -y
+        apt-get install build-essential haveged -y
+        apt-get install software-properties-common -y
+        add-apt-repository ppa:wireguard/wireguard -y
+        apt-get update
+        apt-get install wireguard -y
+
+    elif [ "$DISTRO" == "Debian" ]; then
+        echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
+        printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
+        apt-get update
+        apt-get upgrade -y
+        apt-get dist-upgrade -y
+        apt-get autoremove clean -y
+        apt-get install build-essential haveged -y
+        apt-get install wireguard -y
+
+    elif [ "$DISTRO" == "CentOS" ]; then
+        curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+        yum update -y
+        yum install epel-release -y
+        yum install wireguard-dkms wireguard-tools -y
+    fi
