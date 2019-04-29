c7abd96c616b7bf7efb3ba7f04ee89858a83c844
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-04-29 09:49:22 -0400

    Update wireguard-peer.sh

--- wireguard-peer.sh
+++ wireguard-peer.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 #
 # https://github.com/LiveChief/wireguard-install
-# Secure WireGuard Client installer for Debian, Ubuntu, CentOS
-# https://www.digitalocean.com/community/tutorials/how-to-create-a-point-to-point-vpn-with-wireguard-on-ubuntu-16-04
+# Secure WireGuard server installer for Debian, Ubuntu
+#
+
+WG_CONFIG="/etc/wireguard/wg0.conf"
 
 if [[ "$EUID" -ne 0 ]]; then
     echo "Sorry, you need to run this as root"
     exit
 fi
 
-if [ -e /etc/centos-release ]; then
-    DISTRO="CentOS"
-elif [ -e /etc/debian_version ]; then
+if [[ ! -e /dev/net/tun ]]; then
+    echo "The TUN device is not available. You need to enable TUN before running this script"
+    exit
+fi
+
+if [ -e /etc/debian_version ]; then
     DISTRO=$( lsb_release -is )
 else
     echo "Your distribution is not supported (yet)"
@@ -23,6 +28,129 @@ if [ "$( systemd-detect-virt )" == "openvz" ]; then
     exit
 fi
 
+if [ ! -f "$WG_CONFIG" ]; then
+    ### Install server and add default client
+    INTERACTIVE=${INTERACTIVE:-yes}
+    PRIVATE_SUBNET_V4=${PRIVATE_SUBNET_V4:-"10.8.0.0/24"}
+    PRIVATE_SUBNET_MASK_V4=$( echo $PRIVATE_SUBNET_V4 | cut -d "/" -f 2 )
+    GATEWAY_ADDRESS_V4="${PRIVATE_SUBNET_V4::-4}1"
+    PRIVATE_SUBNET_V6=${PRIVATE_SUBNET_V6:-"fd42:42:42::1/64"}
+    PRIVATE_SUBNET_MASK_V6=$( echo $PRIVATE_SUBNET_V6 | cut -d "/" -f 2 )
+    GATEWAY_ADDRESS_V6="${PRIVATE_SUBNET_V6::-4}1"
+
+    if [ "$SERVER_HOST" == "" ]; then
+        SERVER_HOST="$(wget -O - -q https://checkip.amazonaws.com)"
+        if [ "$INTERACTIVE" == "yes" ]; then
+            read -p "Servers public IP address is $SERVER_HOST. Is that correct? [y/n]: " -e -i "y" CONFIRM
+            if [ "$CONFIRM" == "n" ]; then
+                echo "Aborted. Use environment variable SERVER_HOST to set the correct public IP address"
+                exit
+            fi
+        fi
+    fi
+
+    	echo "What port do you want WireGuard to listen to?"
+	echo "   1) Default: 51820"
+	echo "   2) Custom"
+	echo "   3) Random [2000-65535]"
+	until [[ "$PORT_CHOICE" =~ ^[1-3]$ ]]; do
+		read -rp "Port choice [1-3]: " -e -i 1 PORT_CHOICE
+	done
+	case $PORT_CHOICE in
+		1)
+			SERVER_PORT="51820"
+		;;
+		2)
+			until [[ "$SERVER_PORT" =~ ^[0-9]+$ ]] && [ "$SERVER_PORT" -ge 1 ] && [ "$SERVER_PORT" -le 65535 ]; do
+				read -rp "Custom port [1-65535]: " -e -i 51820 SERVER_PORT
+			done
+		;;
+		3)
+			# Generate random number within private ports range
+			SERVER_PORT=$(shuf -i2000-65535 -n1)
+			echo "Random Port: $SERVER_PORT"
+		;;
+	esac
+	
+    echo "Are you behind a firewall or NAT?"
+    echo "   1) Yes"
+    echo "   2) No"
+    until [[ "$NAT_CHOICE" =~ ^[1-2]$ ]]; do
+        read -rp "Nat Choice [1-2]: " -e -i 2 NAT_CHOICE
+    done
+    case $NAT_CHOICE in
+        1)
+            NAT_CHOICE="25"
+        ;;
+        2)
+            NAT_CHOICE="0"
+        ;;
+    esac
+ 
+    echo "What MTU do you want to use?"
+    echo "   1) 1500"
+    echo "   2) 1420"
+    until [[ "$MTU_CHOICE" =~ ^[1-2]$ ]]; do
+        read -rp "MTU Choice [1-2]: " -e -i 2 MTU_CHOICE
+    done
+    case $MTU_CHOICE in
+        1)
+            MTU_CHOICE="1500"
+        ;;
+        2)
+            MTU_CHOICE="1420"
+        ;;
+    esac
+
+    if [ "$CLIENT_DNS" == "" ]; then
+        echo "Which DNS do you want to use with the VPN?"
+        echo "   1) Cloudflare"
+        echo "   2) Google"
+        echo "   3) OpenDNS"
+        echo "   4) AdGuard"
+        echo "   5) AdGuard Family Protection"
+        echo "   6) Quad9"
+        echo "   7) Quad9 Uncensored"
+        echo "   8) FDN"
+        echo "   9) DNS.WATCH"
+        echo "   10) Yandex Basic"
+        read -p "DNS [1-10]: " -e -i 4 DNS_CHOICE
+
+        case $DNS_CHOICE in
+            1)
+            CLIENT_DNS="1.1.1.1"
+            ;;
+            2)
+            CLIENT_DNS="8.8.8.8"
+            ;;
+            3)
+            CLIENT_DNS="208.67.222.222"
+            ;;
+            4)
+            CLIENT_DNS="176.103.130.130"
+            ;;
+            5)
+            CLIENT_DNS="176.103.130.132"
+            ;;
+            6)
+            CLIENT_DNS="9.9.9.9"
+            ;;
+            7)
+            CLIENT_DNS="9.9.9.10"
+            ;;
+            8)
+            CLIENT_DNS="80.67.169.40"
+            ;;
+            9)
+            CLIENT_DNS="84.200.69.80"
+            ;;
+            10)
+            CLIENT_DNS="77.88.8.8"
+            ;;
+        esac
+        
+    fi
+
     if [ "$DISTRO" == "Ubuntu" ]; then
         apt-get update
         apt-get upgrade -y
@@ -32,21 +160,88 @@ fi
         apt-get install software-properties-common -y
         add-apt-repository ppa:wireguard/wireguard -y
         apt-get update
-        apt-get install wireguard -y
-
+        apt-get install wireguard qrencode iptables-persistent -y
+	    apt-get install unattended-upgrades apt-listchanges -y
+        wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/wireguard-install/master/unattended-upgrades/50unattended-upgrades.Ubuntu"
+        
     elif [ "$DISTRO" == "Debian" ]; then
         echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
         printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
         apt-get update
         apt-get upgrade -y
         apt-get dist-upgrade -y
-        apt-get autoremove clean -y
+        apt-get autoremove -y
         apt-get install build-essential haveged -y
-        apt-get install wireguard -y
-
-    elif [ "$DISTRO" == "CentOS" ]; then
-        curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
-        yum update -y
-        yum install epel-release -y
-        yum install wireguard-dkms wireguard-tools -y
+        apt-get install wireguard qrencode iptables-persistent -y
+	    apt-get install unattended-upgrades apt-listchanges -y
+        wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/wireguard-install/master/unattended-upgrades/50unattended-upgrades.Debian"
     fi
+
+    SERVER_PRIVKEY=$( wg genkey )
+    SERVER_PUBKEY=$( echo $SERVER_PRIVKEY | wg pubkey )
+    CLIENT_PRIVKEY=$( wg genkey )
+    CLIENT_PUBKEY=$( echo $CLIENT_PRIVKEY | wg pubkey )
+    CLIENT_ADDRESS_V4="${PRIVATE_SUBNET_V4::-4}3"
+    CLIENT_ADDRESS_V6="${PRIVATE_SUBNET_V6::-4}3"
+
+    mkdir -p /etc/wireguard
+    touch $WG_CONFIG && chmod 600 $WG_CONFIG
+
+    echo "# $PRIVATE_SUBNET_V4 $PRIVATE_SUBNET_V6 $SERVER_HOST:$SERVER_PORT $SERVER_PUBKEY $MTU_CHOICE $NAT_CHOICE
+[Interface]
+Address = $GATEWAY_ADDRESS_V4/$PRIVATE_SUBNET_MASK_V4, $GATEWAY_ADDRESS_V6/$PRIVATE_SUBNET_MASK_V6
+ListenPort = $SERVER_PORT
+PrivateKey = $SERVER_PRIVKEY
+SaveConfig = false" > $WG_CONFIG
+
+    echo "# client
+[Peer]
+PublicKey = $CLIENT_PUBKEY
+AllowedIPs = $CLIENT_ADDRESS_V4/32, $CLIENT_ADDRESS_V6/128" >> $WG_CONFIG
+
+    echo "[Interface]
+PrivateKey = $CLIENT_PRIVKEY
+Address = $CLIENT_ADDRESS_V4/$PRIVATE_SUBNET_MASK_V4, $CLIENT_ADDRESS_V6/$PRIVATE_SUBNET_MASK_V6
+DNS = 10.8.0.1
+MTU = $MTU_CHOICE
+[Peer]
+PublicKey = $SERVER_PUBKEY
+AllowedIPs = 0.0.0.0/0, ::/0
+Endpoint = $SERVER_HOST:$SERVER_PORT
+PersistentKeepalive = $NAT_CHOICE" > $HOME/client-wg0.conf
+qrencode -t ansiutf8 -l L < $HOME/client-wg0.conf
+
+    echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+    echo "net.ipv4.conf.all.forwarding=1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+    sysctl -p
+
+    if [ "$DISTRO" == "Debian" ]; then	
+        iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT	
+        ip6tables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT	
+        iptables -A FORWARD -m conntrack --ctstate NEW -s $PRIVATE_SUBNET_V4 -m policy --pol none --dir in -j ACCEPT	
+        ip6tables -A FORWARD -m conntrack --ctstate NEW -s $PRIVATE_SUBNET_V6 -m policy --pol none --dir in -j ACCEPT	
+        iptables -t nat -A POSTROUTING -s $PRIVATE_SUBNET_V4 -m policy --pol none --dir out -j MASQUERADE	
+        ip6tables -t nat -A POSTROUTING -s $PRIVATE_SUBNET_V6 -m policy --pol none --dir out -j MASQUERADE	
+        iptables -A INPUT -p udp --dport $SERVER_PORT -j ACCEPT
+        ip6tables -A INPUT -p udp --dport $SERVER_PORT -j ACCEPT
+        iptables-save > /etc/iptables/rules.v4
+    else
+        iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT	
+        ip6tables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT	
+        iptables -A FORWARD -m conntrack --ctstate NEW -s $PRIVATE_SUBNET_V4 -m policy --pol none --dir in -j ACCEPT	
+        ip6tables -A FORWARD -m conntrack --ctstate NEW -s $PRIVATE_SUBNET_V6 -m policy --pol none --dir in -j ACCEPT	
+        iptables -t nat -A POSTROUTING -s $PRIVATE_SUBNET_V4 -m policy --pol none --dir out -j MASQUERADE	
+        ip6tables -t nat -A POSTROUTING -s $PRIVATE_SUBNET_V6 -m policy --pol none --dir out -j MASQUERADE	
+        iptables -A INPUT -p udp --dport $SERVER_PORT -j ACCEPT
+        ip6tables -A INPUT -p udp --dport $SERVER_PORT -j ACCEPT
+        iptables-save > /etc/iptables/rules.v4	
+    fi	
+
+    systemctl enable wg-quick@wg0.service
+    systemctl start wg-quick@wg0.service
+
+    # TODO: unattended updates, apt install dnsmasq ntp
+    echo "Client config --> $HOME/client-wg0.conf"
+    echo "Now reboot the server and enjoy your fresh VPN installation! :^)"
+fi
