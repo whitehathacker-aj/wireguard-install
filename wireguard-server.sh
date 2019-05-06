1ba6747e1e6c32bdeacb01c3b2bcaeae227a89f2
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-05-06 00:06:08 -0400

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -157,14 +157,15 @@ if [ ! -f "$WG_CONFIG" ]; then
         apt-get upgrade -y
         apt-get dist-upgrade -y
         apt-get autoremove -y
-        apt-get install build-essential haveged -y
+        apt-get install build-essential haveged ntpdate -y
         apt-get install software-properties-common -y
         add-apt-repository ppa:wireguard/wireguard -y
         apt-get update
         apt-get install wireguard qrencode iptables-persistent -y
         apt-get install unattended-upgrades apt-listchanges -y
         wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/ubuntu/50unattended-upgrades.Ubuntu"
-	
+	ntpdate pool.ntp.org
+
     elif [ "$DISTRO" == "Debian" ]; then
         echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
         printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
@@ -172,11 +173,12 @@ if [ ! -f "$WG_CONFIG" ]; then
         apt-get upgrade -y
         apt-get dist-upgrade -y
         apt-get autoremove -y
-        apt-get install build-essential haveged -y
+        apt-get install build-essential haveged ntpdate -y
         apt-get install wireguard qrencode iptables-persistent -y
         apt-get install unattended-upgrades apt-listchanges -y
         wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/debian/50unattended-upgrades.Debian"
-    
+	ntpdate pool.ntp.org
+	
     elif [ "$DISTRO" == "CentOS" ]; then
         curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
         yum install epel-release -y
