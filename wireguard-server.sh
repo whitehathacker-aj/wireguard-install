0bbcc9e290453e08f27e2ec29440e6760d88072b
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-05-10 06:26:24 -0400

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -172,7 +172,7 @@ if [ ! -f "$WG_CONFIG" ]; then
         apt-get install software-properties-common -y
         add-apt-repository ppa:wireguard/wireguard -y
         apt-get update
-        apt-get install wireguard qrencode iptables-persistent unattended-upgrades apt-listchanges haveged ntpdate  linux-headers-$(uname -r) -y
+        apt-get install wireguard qrencode iptables-persistent unattended-upgrades apt-listchanges haveged ntpdate  linux-headers-$(uname -r) build-essential -y
         wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/ubuntu/50unattended-upgrades.Ubuntu"
 	ntpdate pool.ntp.org
 	
@@ -180,7 +180,7 @@ if [ ! -f "$WG_CONFIG" ]; then
         echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
         printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
         apt-get update
-        apt-get install wireguard qrencode iptables-persistent unattended-upgrades apt-listchanges haveged ntpdate  linux-headers-$(uname -r) -y
+        apt-get install wireguard qrencode iptables-persistent unattended-upgrades apt-listchanges haveged ntpdate  linux-headers-$(uname -r) build-essential -y
         wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/debian/50unattended-upgrades.Debian"
 	ntpdate pool.ntp.org
 	
