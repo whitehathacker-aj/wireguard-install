23d2c3495c2e1787a3597730b172817feb028a1b
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-05-10 23:09:29 -0400

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -144,9 +144,9 @@ if [ "$SERVER_HOST_V6" == "" ]; then
             DISABLE_HOST=""
         ;;
         3)
-            DISABLE_HOST="echo 'net.ipv6.conf.all.disable_ipv6 = 1 
-	    net.ipv6.conf.default.disable_ipv6 = 1 
-	    net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf"
+            DISABLE_HOST="echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
+	    echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
+	    echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf"
         ;;
     esac
     echo "What traffic do you want the client to forward to wireguard?"
