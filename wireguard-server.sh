88929307baa3613209fbc7ad58c3ecd191437b74
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-05-11 00:53:57 -0400

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -148,8 +148,11 @@ if [ "$SERVER_HOST_V6" == "" ]; then
             DISABLE_HOST=""
         ;;
         3)
-            DISABLE_HOST=""
-        ;;
+            DISABLE_HOST="echo 'net.ipv6.conf.all.disable_ipv6 = 1
+	net.ipv6.conf.default.disable_ipv6 = 1
+	net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
+	sysctl -p"
+	;;
     esac
     echo "What traffic do you want the client to forward to wireguard?"
     echo "   1) Everything (Recommended)"
@@ -223,6 +226,7 @@ if [ "$SERVER_HOST_V6" == "" ]; then
         apt-get install wireguard qrencode iptables-persistent unattended-upgrades apt-listchanges haveged ntpdate linux-headers-$(uname -r) -y
         wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/ubuntu/50unattended-upgrades.Ubuntu"
 	ntpdate pool.ntp.org
+	DISABLE_HOST
 	
     elif [ "$DISTRO" == "Debian" ]; then
         echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
@@ -231,6 +235,7 @@ if [ "$SERVER_HOST_V6" == "" ]; then
         apt-get install wireguard qrencode iptables-persistent unattended-upgrades apt-listchanges haveged ntpdate linux-headers-$(uname -r) -y
         wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/debian/50unattended-upgrades.Debian"
 	ntpdate pool.ntp.org
+	DISABLE_HOST
 	
     elif [ "$DISTRO" == "CentOS" ]; then
         curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
@@ -275,7 +280,7 @@ qrencode -t ansiutf8 -l L < $HOME/client-wg0.conf
     echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
     echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
     sysctl -p
-
+    
     if [ "$DISTRO" == "CentOS" ]; then
         systemctl start firewalld
         firewall-cmd --zone=public --add-port=$SERVER_PORT/udp
