97bd94344326b1eda49f224406c8e0679f03a9b4
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-05-11 10:25:45 -0400

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -144,9 +144,9 @@ if [ "$SERVER_HOST_V6" == "" ]; then
             DISABLE_HOST="sysctl -p"
         ;;
         2)
-            DISABLE_HOST="(echo 'net.ipv6.conf.all.disable_ipv6 = 1
-	    net.ipv6.conf.default.disable_ipv6 = 1
-	    net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
+            DISABLE_HOST="(echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
+	    echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
+	    echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
 	    sysctl -p)"
         ;;
     esac
