4c146fb988347f0c9816b28cfd8e47da158bd89f
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-05-14 22:17:59 -0400

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -13,6 +13,10 @@ if [ -e /etc/centos-release ]; then
     DISTRO="CentOS"
 elif [ -e /etc/debian_version ]; then
     DISTRO=$( lsb_release -is )
+elif [[ -e /etc/arch-release ]]; then
+    DISTRO="Arch"
+elif [[ -e /etc/fedora-release ]]; then
+    DISTRO="Fedora"
 else
     echo "Your distribution is not supported (yet)"
     exit
@@ -220,18 +224,35 @@ if [ "$SERVER_HOST_V6" == "" ]; then
         apt-get update
         apt-get install wireguard qrencode ntpdate linux-headers-$(uname -r) ntpdate -y
 	ntpdate pool.ntp.org
+	echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+	echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+	$DISABLE_HOST
 
     elif [ "$DISTRO" == "Debian" ]; then
-        echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
-        printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
-        apt-get update
-        apt-get install wireguard qrencode linux-headers-$(uname -r) ntpdate -y
+	echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
+	printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
+	apt-get update -y
+        apt-get install wireguard qrencode ntpdate linux-headers-$(uname -r) ntpdate -y
 	ntpdate pool.ntp.org
+	echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+	echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+	$DISABLE_HOST
+	
+    elif [ "$DISTRO" == "Arch" ]; then
+	pacman -Syy
+	pacman -S wireguard-tools
+
+    elif [[ "$DISTRO" = 'Fedora' ]]; then
+	dnf update -y
+	dnf copr enable jdoss/wireguard -y
+	dnf install wireguard-dkms wireguard-tools -y
 
     elif [ "$DISTRO" == "CentOS" ]; then
-        wget -O /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
-        yum install epel-release -y
-        yum install wireguard-dkms qrencode wireguard-tools ntpdate -y
+	yum update -y
+	wget -O /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+	yum install epel-release -y
+	yum install wireguard-dkms wireguard-tools qrencode ntpdate -y
+    
     fi
 
     SERVER_PRIVKEY=$( wg genkey )
