a52f705f02f8228cca521490eb348a9750e5249e
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-05-16 20:29:59 -0400

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -248,7 +248,6 @@ if [ "$SERVER_HOST_V6" == "" ]; then
 
     elif [ "$DISTRO" = 'Fedora' ]; then
 	dnf update -y
-	dnf upgrade -y
 	dnf copr enable jdoss/wireguard -y
 	dnf install qrencode ntpdate ernel-headers-$(uname -r) kernel-devel-$(uname -r) wireguard-dkms wireguard-tools -y
 
