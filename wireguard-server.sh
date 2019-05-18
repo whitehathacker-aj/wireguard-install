661e6fabef818210a09c1c7d14a74a8a7f7c0fc5
Live Chief <16564273+LiveChief@users.noreply.github.com>
2019-05-18 16:52:47 -0400

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -255,16 +255,16 @@ if [ "$SERVER_HOST_V6" == "" ]; then
 	$DISABLE_HOST
 	
     elif [ "$DISTRO" == "CentOS" ]; then
-	wget -O /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
 	yum update -y
+	wget -O /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
 	yum install epel-release wireguard-dkms wireguard-tools qrencode ntpdate kernel-headers-$(uname -r) kernel-devel-$(uname -r) haveged -y
 	echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 	echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
 	$DISABLE_HOST
 	
     elif [ "$DISTRO" == "Redhat" ]; then
-	wget -O /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
 	yum update -y
+	wget -O /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
 	yum install epel-release -y
 	yum install wireguard-dkms wireguard-tools qrencode ntpdate kernel-headers-$(uname -r) kernel-devel-$(uname -r) haveged -y
 	echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
