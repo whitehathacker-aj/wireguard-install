bc1e41fba01ca871ae4917488fe754b16295a0cc
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-06-08 13:51:04 -0400

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -273,11 +273,11 @@ if [ "$SERVER_HOST_V6" == "" ]; then
 
     elif [ "$DISTRO" == "Raspbian" ]; then
 	apt-get update
+	apt-get install raspberrypi-kernel-headers
 	echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
-	printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
-	apt-get update
 	apt-get install dirmngr -y
-        apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
+	printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
 	apt-get install wireguard qrencode ntpdate haveged raspberrypi-kernel-headers -y
 	echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 	echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
